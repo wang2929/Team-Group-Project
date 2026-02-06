dd8632ed42661a6437ae83adf98d184b89dc9755
wang2929 <tifalsuk@gmail.com>
2026-02-07 01:28:19 +0300

    added a function to run the phone terminal app

---
+++ phone-terminal.py
@@ -0,0 +1,6 @@
+# Placeholder import because will import main-menu later
+from main_menu import main_menu
+
+# Putting main here, though in main-menu also works, just because could be better design-wise
+if __name__ == '__main__':
+    main_menu()
